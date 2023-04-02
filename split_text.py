19b779215e8b4c59158972ffdc8f31ccffef4f02
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-04-02 09:45:00 -0400

    attempting to figure out errors

--- split_text.py
+++ split_text.py
@@ -1,6 +1,20 @@
-romeo = '''
-But soft what light through yonder window breaks
-It is the east and Juliet is the sun
-Arise fair sun and kill the envious moon
-Who is already sick and pale with grief
-'''
+import mbox_short
+
+fname = mbox_short.mboxShort
+
+#if len(fname) < 1:
+#    fname = "mbox-short.txt"
+
+#fh = open(fname)
+count = 0
+
+for line in fname:
+    if line.startswith("From"): ###this line needs to be fixed
+        words = line.split()
+        print(words[2])
+        count += 1
+
+    else:
+        continue
+
+print(f"There were", {count}, "lines in the file with From as the first word")
