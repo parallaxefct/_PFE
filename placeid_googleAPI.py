cfff1d862c3ef9e5092c9f479bb8acd51328ff02
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-05-21 15:50:43 -0400

    Update placeid_googleAPI.py

--- placeid_googleAPI.py
+++ placeid_googleAPI.py
@@ -1,4 +1,19 @@
 #enter a location / parse the returned JSON data and retrieve the PLACE_ID
 import urllib.request, urllib.parse, urllib.error
-import ssl
 import json
+
+
+serviceurl = 'http://py4e-data.dr-chuck.net/json?' #provide the intial url
+
+
+address = input('Enter Location: ') #prompt for location
+ad_key = {'address' : address, 'key' : 42} #create a dictionary and create key:value for address and key
+
+url = serviceurl + urllib.parse.urlencode(ad_key) #encode location and concantenate to serviceurl
+handle = urllib.request.urlopen(url) #create handle
+data = handle.read().decode() #decode utf-8
+
+
+peeled = json.loads(data) #convert data to dictionary
+
+print(peeled['results'][0]['place_id'])
